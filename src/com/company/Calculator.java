f6e28e76d3b37c875afbb110874440f3046d5f72
Martin <martbelmoaw@gmail.com>
2018-08-29 15:44:58 +0300

    Added full "multiply" and "division" functionality

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -45,7 +45,10 @@ class Calculator {
         System.out.println("\n\n///Menu\\\\\\");
         System.out.println("1. Adding");
         System.out.println("2. Subtraction");
-        System.out.println("3. Exit");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("5. Fill with another array");
+        System.out.println("6. Exit");
 
         int selector = 0;
 
@@ -59,7 +62,7 @@ class Calculator {
         String response;
 
         switch (selector){
-            case 3:
+            case 6:
                 System.exit(0);
             case 1:
                 input_indexes = new ArrayList<>();
@@ -105,6 +108,55 @@ class Calculator {
 
                 System.out.println(response + subtract(input_indexes) + "\n");
                 break;
+            case 3:
+                input_indexes = new ArrayList<>();
+
+                System.out.println("Enter indexes of numbers for 'multiplication' operation (press Enter to calculate all numbers):");
+                while(true){
+                    try {
+                        String context = this.reader.readLine();
+                        if(context.equals("")){
+                            break;
+                        }
+
+                        input_indexes.add(Integer.parseInt(context));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                response = (input_indexes.size() != 0) ?
+                        "Multiplication of selected numbers: " : "Multiplication of all numbers: ";
+
+                System.out.println(response + multiply(input_indexes) + "\n");
+                break;
+            case 4:
+                input_indexes = new ArrayList<>();
+
+                System.out.println("Enter indexes of numbers for 'division' operation (press Enter to calculate all numbers):");
+                while(true){
+                    try {
+                        String context = this.reader.readLine();
+                        if(context.equals("")){
+                            break;
+                        }
+
+                        input_indexes.add(Integer.parseInt(context));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                response = (input_indexes.size() != 0) ?
+                        "Division of selected numbers: " : "Division of all numbers: ";
+
+                System.out.println(response + division(input_indexes) + "\n");
+                break;
+            case 5:
+                fill();
+                break;
+            default:
+                System.out.println("Unknown command...");
         }
     }
 
@@ -150,5 +202,54 @@ class Calculator {
         }
 
         return sub;
+    }
+
+    //Multiply of numbers
+    private int multiply(ArrayList<Integer> indexes) {
+        int sum = 1;
+
+        if (indexes.size() > 0) {
+            for (int index : indexes) {
+                if (index > (this.numbers.size() - 1)) {
+                    continue;
+                }
+
+                sum *= this.numbers.get(index);
+            }
+        } else {
+            for(int num : this.numbers){
+                sum *= num;
+            }
+        }
+
+        return sum;
+    }
+
+    //Division of numbers
+    private int division(ArrayList<Integer> indexes) {
+        int div = this.numbers.get(0);
+
+        if (indexes.size() > 0) {
+            div = this.numbers.get(indexes.get(0));
+
+            for (int index = 1; index < indexes.size(); index++) {
+                if ((index > (this.numbers.size() - 1))
+                        || (this.numbers.get(indexes.get(index)) == 0)) {
+                    continue;
+                }
+
+                div /= this.numbers.get(indexes.get(index));
+            }
+        } else {
+            for(int num = 1; num < numbers.size(); num++){
+                if(this.numbers.get(num) == 0){
+                    continue;
+                }
+
+                div /= this.numbers.get(num);
+            }
+        }
+
+        return div;
     }
 }
