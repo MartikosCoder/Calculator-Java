53af28a6bbc2001bf4704d3f39a01a90cb2e4870
Martin <martbelmoaw@gmail.com>
2018-08-29 14:34:40 +0300

    Added "add" function

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -9,4 +9,17 @@ public class Calculator {
         this.numbers = input;
     }
 
+    int add(ArrayList<Integer> indexes) {
+        int sum = 0;
+
+        for(int index : indexes){
+            if(index > (this.numbers.size() - 1)){
+                continue;
+            }
+
+            sum += this.numbers.get(index);
+        }
+
+        return sum;
+    }
 }
