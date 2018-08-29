7a0631498a84458c464fdafb569238c9c86ed01b
Martin <martbelmoaw@gmail.com>
2018-08-29 14:10:38 +0300

    First commit

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Calculator {
+    private ArrayList<Integer> numbers;
+
+    Calculator(ArrayList<Integer> input){
+        this.numbers = input;
+    }
+    
+}
